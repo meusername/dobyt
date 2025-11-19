ac884042b7354f0dbac673ed91bd1e54edab9d6b
meusername <6240768@gmail.com>
2025-11-19 22:07:11 +0300

    logging fix

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ import psycopg2
 from dotenv import load_dotenv
 
 # Настройка логирования
-ogging.basicConfig(
+logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
